426fe4aa0a98f24b2f24a7ac22d558ec015ab206
Amresh <amresh.kumar@photoninfotech.net>
2019-06-17 16:12:24 +0530

    All working till lab 5 end

---
+++ events.tac.local/Controllers/EventListController.cs
@@ -0,0 +1,28 @@
+﻿using events.tac.local.Models;
+using Sitecore.Mvc.Presentation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace events.tac.local.Controllers
+{
+  public class EventListController : Controller
+  {
+    // GET: EventList
+    public ActionResult Index()
+    {
+      return View("~/Views/Events/EventList/Index.cshtml", CreateModel());
+    }
+
+    public static EventList CreateModel()
+    {
+      var item = RenderingContext.Current.ContextItem;
+      return new EventList()
+      {
+        
+      };
+    }
+  }
+}
